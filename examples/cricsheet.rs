078cf8376b2339edf502670ba402b07bfa1baf85
David <david.14587@gmail.com>
2025-08-31 23:00:13 +1000

    Refactor cricsheet display code

--- examples/cricsheet.rs
+++ examples/cricsheet.rs
@@ -1,8 +1,9 @@
 mod cricsheet_lib;
 #[path = "shared/cricsheet_utils.rs"]
 mod cricsheet_utils;
-use cricsheet_lib::Event;
-use cricsheet_utils::{compare_results, get_cricsheet_game};
+use cricsheet_utils::{
+    compare_results, format_cricsheet_result, format_result, get_cricsheet_game, print_diffs,
+};
 
 fn main() {
     // parse a set of cricsheet games
@@ -29,105 +30,29 @@ fn main() {
 
         cricket_match.score(cricsheet.info.outcome.create_outcome());
 
-        let by = match cricsheet.info.outcome.by {
-            Some(x) => format!("{}", &x),
-            None => cricsheet.info.outcome.result.clone().unwrap_or_default(),
-        };
-        let cricsheet_result = format!(
-            "{} {by} {}",
-            cricsheet
-                .info
-                .outcome
-                .winner
-                .clone()
-                .unwrap_or("NO WINNER".to_string()),
-            cricsheet.info.outcome.method.clone().unwrap_or_default()
-        );
+        let cricsheet_result = format_cricsheet_result(&cricsheet);
 
-        let res = cricket_match.outcome.unwrap();
-        let mut innings_win_text = String::new();
-        if res.innings_win {
-            innings_win_text = String::from("an innings and ");
-        }
-        let draw_win;
-        let mut margin = String::new();
-        if res.draw {
-            draw_win = "Draw";
-        } else if res.tie {
-            draw_win = "tie";
-        } else if !res.result {
-            draw_win = "no result";
-        } else if res.method.clone().unwrap_or_default() == "Awarded" {
-            draw_win = "";
-        } else if res.method.clone().unwrap_or_default() == "Lost fewer wickets" {
-            draw_win = "";
-        } else {
-            draw_win = "Won by";
-        };
-        let mut run_wickets = String::new();
-        if res.runs_margin.is_some() {
-            run_wickets = String::from("runs");
-            margin = format!("{}", res.runs_margin.unwrap());
-        };
-        if res.wickets_margin.is_some() {
-            run_wickets = String::from("wickets");
-            margin = format!("{}", res.wickets_margin.unwrap());
-        };
-        let my_result = format!(
-            "{} {draw_win} {}{} {} {}",
-            res.winner.unwrap_or("NO WINNER".to_string()),
-            innings_win_text,
-            margin,
-            run_wickets,
-            res.method.clone().unwrap_or_default(),
-        );
+        let my_result = format_result(&cricket_match);
+
         if my_result
-            .trim_end()
+            .replace(" ", "")
             .strip_suffix('0')
             .unwrap_or(&my_result)
-            .trim_end()
             .to_lowercase()
-            .replace("   ", " ")
-            == cricsheet_result.trim_end().to_lowercase()
+            == cricsheet_result.trim_end().to_lowercase().replace(" ", "")
         {
             correct_result += 1;
         } else {
-            println!("---------------------------------");
-            println!(
-                "{:?} {}",
-                cricsheet.info.dates,
-                cricsheet
-                    .info
-                    .event
-                    .unwrap_or_else(|| Event {
-                        name: String::new(),
-                        ..Default::default()
-                    })
-                    .name
+            print_diffs(
+                &cricsheet,
+                &cricket_match,
+                &cricsheet_result,
+                &my_result,
+                file.expect(""),
             );
-            println!(
-                "MATCH {}\nCRICSHEET: {}\nMY SCORE: {}",
-                file.unwrap().file_name().into_string().unwrap(),
-                cricsheet_result,
-                my_result
-            );
-            println!(
-                "{}",
-                cricket_match
-                    .innings
-                    .iter()
-                    .map(|x| x.score.summary())
-                    .fold(String::new(), |x, y| vec![x, "\n".to_string(), y]
-                        .into_iter()
-                        .collect::<String>())
-            );
-            // println!("{:?} {:?}", cricsheet.info.outcome.method, cricsheet.info.outcome.result);
         }
 
         read_files += 1;
-        // if cricsheet.info.outcome.result == Some(String::from("no result")) {
-        //     break;
-        // }
     }
     println!(
         "Got {correct_result} right out of {read_files} games ({:.2}%)",
--- examples/shared/cricsheet_utils.rs
+++ examples/shared/cricsheet_utils.rs
@@ -1,7 +1,8 @@
 use std::fs::{DirEntry, File};
 use std::io::Read;
 
-use crate::cricsheet_lib::{Cricsheet, CricsheetInnings};
+use crate::cricsheet_lib::{Cricsheet, CricsheetInnings, Event};
+use cricket_scoring::scoring::game::Game;
 use cricket_scoring::scoring::innings::Innings;
 
 pub fn compare_results(innings_data: &CricsheetInnings, innings: &Innings) {
@@ -87,4 +88,100 @@ pub fn get_cricsheet_game(directory_entry: &DirEntry) -> Option<Cricsheet> {
     // parse the game into a Cricsheet object
     let json: serde_json::Value = serde_json::from_str(&data).unwrap();
     Some(serde_json::from_value(json).unwrap())
+}
+
+pub fn format_cricsheet_result(cricsheet: &Cricsheet) -> String {
+    let by = match cricsheet.info.outcome.by {
+        Some(x) => format!("{}", &x),
+        None => cricsheet.info.outcome.result.clone().unwrap_or_default(),
+    };
+    format!(
+        "{} {by} {}",
+        cricsheet
+            .info
+            .outcome
+            .winner
+            .clone()
+            .unwrap_or("NO WINNER".to_string()),
+        cricsheet.info.outcome.method.clone().unwrap_or_default()
+    )
+}
+
+pub fn format_result(cricket_match: &Game) -> String {
+    let res = cricket_match.outcome.as_ref().unwrap();
+    let mut innings_win_text = String::new();
+    if res.innings_win {
+        innings_win_text = String::from("an innings and ");
+    }
+    let draw_win;
+    let mut margin = String::new();
+    if res.draw {
+        draw_win = "Draw";
+    } else if res.tie {
+        draw_win = "tie";
+    } else if !res.result {
+        draw_win = "no result";
+    } else if res.method.clone().unwrap_or_default() == "Awarded" {
+        draw_win = "";
+    } else if res.method.clone().unwrap_or_default() == "Lost fewer wickets" {
+        draw_win = "";
+    } else {
+        draw_win = "Won by";
+    };
+    let mut run_wickets = String::new();
+    if res.runs_margin.is_some() {
+        run_wickets = String::from("runs");
+        margin = format!("{}", res.runs_margin.unwrap());
+    };
+    if res.wickets_margin.is_some() {
+        run_wickets = String::from("wickets");
+        margin = format!("{}", res.wickets_margin.unwrap());
+    };
+    format!(
+        "{} {draw_win} {}{} {} {}",
+        res.winner.clone().unwrap_or("NO WINNER".to_string()),
+        innings_win_text,
+        margin,
+        run_wickets,
+        res.method.clone().unwrap_or_default(),
+    )
+}
+
+pub fn print_diffs(
+    cricsheet: &Cricsheet,
+    cricket_match: &Game,
+    cricsheet_result: &str,
+    my_result: &str,
+    file: DirEntry,
+) {
+    println!("---------------------------------");
+    println!(
+        "{:?} {}",
+        cricsheet.info.dates,
+        cricsheet
+            .info
+            .event
+            .clone()
+            .unwrap_or_else(|| Event {
+                name: String::new(),
+                ..Default::default()
+            })
+            .name
+    );
+    println!(
+        "MATCH {}\nCRICSHEET: {}\nMY SCORE: {}",
+        file.file_name().into_string().unwrap(),
+        cricsheet_result,
+        my_result
+    );
+    println!(
+        "{}",
+        cricket_match
+            .innings
+            .iter()
+            .map(|x| x.score.summary())
+            .fold(String::new(), |x, y| vec![x, "\n".to_string(), y]
+                .into_iter()
+                .collect::<String>())
+    );
 }
